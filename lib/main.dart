e13b434e47144dbf66db6f425b72d3b048f7cc48
Arthurlima544 <arthurlindaolima544@gmail.com>
2025-08-20 10:49:03 -0300

    feat(presentation): show Home page as first page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'presentation/pages/home_page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,6 +10,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => const MaterialApp(
-    home: Scaffold(body: Center(child: Text('Hello World!'))),
+    home: Scaffold(body: Center(child: Home())),
   );
 }
