3ef1297b554d318334495da50e99981e49c6fc25
Arthurlima544 <arthurlindaolima544@gmail.com>
2025-08-20 14:12:08 -0300

    feat(config): IOS configs

--- lib/presentation/home/pages/home_page.dart
+++ lib/presentation/home/pages/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../domain/model/place/place.dart';
+import '../bloc/home_bloc.dart';
 import '../widgets/home_card.dart';
 
 class Home extends StatelessWidget {
@@ -11,22 +14,44 @@ class Home extends StatelessWidget {
       title: const Text('Skill Playground'),
       backgroundColor: Colors.blue,
     ),
-    body: GridView.count(
-      crossAxisCount: 2,
-      padding: const EdgeInsets.only(left: 20, right: 20, top: 10),
-      crossAxisSpacing: 15,
-      mainAxisSpacing: 15,
-      children: const <Widget>[
-        HomeCard(title: 'CustomPainter Basics'),
-        HomeCard(title: 'Navigator 2.0/ RouterDelegate'),
-        HomeCard(title: 'Bloc State Management'),
-        HomeCard(title: 'Platform Channels'),
-        HomeCard(title: 'Performance & Repaints'),
-        HomeCard(title: 'Async Isolates'),
-        HomeCard(title: 'AnimationController Lifecycle'),
-        HomeCard(title: 'StreamBuilder Error Handling'),
-        HomeCard(title: 'Custom Slivers'),
-      ],
+    body: BlocBuilder<HomeBloc, HomeState>(
+      builder: (BuildContext context, HomeState state) => state.when(
+        initial: () => const Center(child: Text('Inicializando..')),
+        loading: () => const Center(child: CircularProgressIndicator()),
+        success: (List<Place> places) => GridView.builder(
+          padding: const EdgeInsets.only(left: 20, right: 20, top: 10),
+          itemCount: places.length,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 15,
+            mainAxisSpacing: 15,
+          ),
+          itemBuilder: (BuildContext context, int index) =>
+              HomeCard(title: places[index].title, place: places[index]),
+        ),
+        error: (String message) => Center(child: Text(message)),
+      ),
     ),
   );
 }
+
+
+
+// GridView.count(
+//         itemCount: state,
+//         crossAxisCount: 2,
+//         padding: const EdgeInsets.only(left: 20, right: 20, top: 10),
+//         crossAxisSpacing: 15,
+//         mainAxisSpacing: 15,
+//         children: const <Widget>[
+//           HomeCard(title: 'CustomPainter Basics'),
+//           HomeCard(title: 'Navigator 2.0/ RouterDelegate'),
+//           HomeCard(title: 'Bloc State Management'),
+//           HomeCard(title: 'Platform Channels'),
+//           HomeCard(title: 'Performance & Repaints'),
+//           HomeCard(title: 'Async Isolates'),
+//           HomeCard(title: 'AnimationController Lifecycle'),
+//           HomeCard(title: 'StreamBuilder Error Handling'),
+//           HomeCard(title: 'Custom Slivers'),
+//         ],
+//       ),
