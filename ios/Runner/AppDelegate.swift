c30db378fe841d62c7792208d6caf5e1461ef390
Arthurlima544 <arthurlindaolima544@gmail.com>
2025-08-21 07:24:16 -0300

    feat(ios): create getOSVersion  and config files

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -5,8 +5,25 @@ import UIKit
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+    guard let controller = window?.rootViewController as? FlutterViewController else {
+      fatalError("rootViewController is not a FlutterViewController")
+    }
+
+    let channel = FlutterMethodChannel(name: "samples.flutter.dev/info",
+                                       binaryMessenger: controller.binaryMessenger)
+
+    channel.setMethodCallHandler({
+      (call: FlutterMethodCall, result: FlutterResult) -> Void in
+      
+      if call.method == "getOSVersion" {
+        result(UIDevice.current.systemVersion)
+      } else {
+        result(FlutterMethodNotImplemented)
+      }
+    })
+    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
