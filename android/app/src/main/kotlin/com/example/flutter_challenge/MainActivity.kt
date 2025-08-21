af2e03081d670e42918ff5995fc10d1266db2137
Arthurlima544 <arthurlindaolima544@gmail.com>
2025-08-21 07:21:52 -0300

    feat(android): create getOSVersion function to retrive version number from android

--- android/app/src/main/kotlin/com/example/flutter_challenge/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_challenge/MainActivity.kt
@@ -1,5 +1,23 @@
 package com.example.flutter_challenge
 
+import android.os.Build
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+  private val CHANNEL = "samples.flutter.dev/info"
+
+  override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+    super.configureFlutterEngine(flutterEngine)
+    MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+      call, result ->
+      if (call.method == "getOSVersion") {
+        result.success("Android ${Build.VERSION.RELEASE}")
+      } else {
+        result.notImplemented()
+      }
+    }
+  }
+}
