030446843f606de0a4e6413f715aed0aa5395d24
Arthurlima544 <arthurlindaolima544@gmail.com>
2025-08-21 08:46:58 -0300

    feat(presentation): add appbar backbutton

--- lib/presentation/plataform_channels/pages/plataform_channels_page.dart
+++ lib/presentation/plataform_channels/pages/plataform_channels_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../utils/routes/skill_playground_router_delegate.dart';
 import '../bloc/plataform_channels_bloc.dart';
 
 class PlataformChannelsPage extends StatelessWidget {
@@ -8,6 +9,14 @@ class PlataformChannelsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(
+      leading: BackButton(
+        onPressed: () {
+          (Router.of(context).routerDelegate as SkillPlaygroundRouterDelegate)
+              .goHome();
+        },
+      ),
+    ),
     body: Center(
       child: BlocBuilder<PlataformChannelsBloc, PlataformChannelsState>(
         builder: (BuildContext context, PlataformChannelsState state) =>
