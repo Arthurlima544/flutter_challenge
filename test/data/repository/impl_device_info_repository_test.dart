d789c9e8ed860011ecf7dc889a48d98d26d7c655
Arthurlima544 <arthurlindaolima544@gmail.com>
2025-09-03 19:32:38 -0300

    test: caching in device info repository regression tests

--- test/data/repository/impl_device_info_repository_test.dart
+++ test/data/repository/impl_device_info_repository_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_challenge/data/exceptions/data_exceptions.dart';
 import 'package:flutter_challenge/data/repository/impl_device_info_repository.dart';
+import 'package:flutter_challenge/domain/entity/device_info_entity.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:result_dart/result_dart.dart';
@@ -25,6 +26,32 @@ void main() {
           .fetchPlatformVersion();
       expect(result.isError(), true);
       expect(result.exceptionOrNull(), isA<NotImplementedPluginException>());
+    });
+
+    test('Should cache platform version when found and reuse it', () async {
+      when(() => mockDeviceInfoService.fetchOsVersion()).thenAnswer(
+        (_) async => const Success(DeviceInfoEntity(osVersion: '17.5')),
+      );
+
+      final ResultDart<String, Exception> result = await deviceInfoRepository
+          .fetchPlatformVersion();
+
+      final DeviceInfoEntity? firstDeviceCached =
+          deviceInfoRepository.cachedDeviceInfo;
+
+      expect(result.isSuccess(), true);
+      expect(result.getOrNull(), '17.5');
+      expect(firstDeviceCached, isA<DeviceInfoEntity>());
+      expect(firstDeviceCached?.osVersion, '17.5');
+
+      await deviceInfoRepository.fetchPlatformVersion();
+
+      final DeviceInfoEntity? reusedDeviceCached =
+          deviceInfoRepository.cachedDeviceInfo;
+
+      verify(() => mockDeviceInfoService.fetchOsVersion()).called(2);
+
+      expect(reusedDeviceCached, equals(firstDeviceCached));
     });
   });
 }
