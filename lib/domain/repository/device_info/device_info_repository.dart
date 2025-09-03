095fed5de9e6cf9a400eab087d112f837beb0fcb
Arthurlima544 <arthurlindaolima544@gmail.com>
2025-09-03 14:54:59 -0300

    fix: removing old stuff

--- lib/domain/repository/device_info/device_info_repository.dart
+++
@@ -1,6 +0,0 @@
-import '../../../utils/result.dart';
-import '../../model/device_info/device_info.dart';
-
-abstract class DeviceInfoRepository {
-  Future<Result<DeviceInfo>> getPlatformVersion();
-}
--- lib/domain/repository/place/place_repository.dart
+++
@@ -1,6 +0,0 @@
-import '../../../utils/result.dart';
-import '../../model/place/place.dart';
-
-abstract class PlaceRepository {
-  Future<Result<List<Place>>> getPlaces();
-}
